e68813ef5932562debd864f6a2bf567f519307a6
Aman-Mehta69 <uncutlyric@gmail.com>
2025-08-23 14:22:44 +0545

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,37 @@
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-// export default defineConfig({
-//     base : "/lenis/",
-//   plugins: [
-//     tailwindcss(),
-//   ],
-// })
-
-
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/lenis/',
-
+    base : "/lenis/",
   plugins: [
     tailwindcss(),
   ],
+})
 
-  build: {
-    chunkSizeWarningLimit: 1000,
 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('three')) return 'three';
-            if (id.includes('@react-three')) return 'r3f';
-            if (id.includes('react') || id.includes('react-dom')) return 'react'; // ✅ Fix here
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
-});
+// import { defineConfig } from 'vite';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//   base: '/lenis/',
+
+//   plugins: [
+//     tailwindcss(),
+//   ],
+
+//   build: {
+//     chunkSizeWarningLimit: 1000,
+
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             if (id.includes('three')) return 'three';
+//             if (id.includes('@react-three')) return 'r3f';
+//             if (id.includes('react') || id.includes('react-dom')) return 'react'; // ✅ Fix here
+//             return 'vendor';
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
